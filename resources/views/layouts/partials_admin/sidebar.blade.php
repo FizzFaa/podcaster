0ee7eafd8c1df965bfb3f174b3e4c4dac544e99d
FizzFaa <apexlegendsisracistt@gmail.com>
2021-06-19 23:52:36 +0500

    routes_created

--- resources/views/layouts/partials_admin/sidebar.blade.php
+++ resources/views/layouts/partials_admin/sidebar.blade.php
@@ -2,7 +2,22 @@
             <div class="card">
                 <div class="card-header">{{ __('Side Bar') }}</div>
                 <div class="card-body">
-                   
+                   <li>
+                   <a href="{{route('categories')}}">{{__('Categories')}}</a>
+                   </li>
+                   <li>
+                   <a href="{{route('videos')}}">{{__('Videos')}}</a>
+                   </li>
+                   <li>
+                   <a href="{{route('podcasts')}}">{{__('Podcasts')}}</a>
+                   </li>
+                   <li>
+                   <a href="{{route('posts')}}">{{__('Posts')}}</a>
+                   </li>
+                   <li>
+                   <a href="{{route('events')}}">{{__('Events')}}</a>
+                   </li>
+
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,11 @@ Auth::routes(['verify'=>true]);
 // Route::group(['middleware'=>['verified']],function(){
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/profile', [App\Http\Controllers\AdminController::class,'manageprofile'])->name('profile');
+    Route::get('/categories', [App\Http\Controllers\AdminController::class,'manageprofile'])->name('categories');
+    Route::get('/posts', [App\Http\Controllers\AdminController::class,'manageprofile'])->name('posts');
+    Route::get('/podcasts', [App\Http\Controllers\AdminController::class,'manageprofile'])->name('podcasts');
+    Route::get('/events', [App\Http\Controllers\AdminController::class,'manageprofile'])->name('events');
+    Route::get('/videos', [App\Http\Controllers\AdminController::class,'manageprofile'])->name('videos');
 	Route::post('/profile', [App\Http\Controllers\AdminController::class,'updateprofile'])->name('updateprofile');
 // });
 
